3646dba5891f167ea67c913178f33271590eceb1
uyenl99 <uyenl99@yahoo.com>
2026-06-02 16:16:48 -0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,34 +1,19 @@
 import os
 import requests
-import pandas as pd
-import plotly.graph_objects as go
 
 API_KEY = os.environ["C2_API_KEY"]
-
-STRATEGY_ID = 13202557
 
 headers = {
     "Authorization": f"Bearer {API_KEY}"
 }
 
-url = (
-    f"https://api.collective2.com/world/apiv4/"
-    f"strategy/{STRATEGY_ID}/historical-equity"
-)
+url = "https://api.collective2.com/world/apiv4"
 
 r = requests.get(url, headers=headers)
 
-print("Status:", r.status_code)
+print("STATUS:", r.status_code)
+print("TEXT:")
+print(r.text[:500])
 
-data = r.json()
-
-print(data)
-
-# temporary page so we know script ran
-html = """
-<h1>Collective2 Connection Successful</h1>
-<p>If you see this page, GitHub Actions ran.</p>
-"""
-
-with open("index.html", "w", encoding="utf-8") as f:
-    f.write(html)
+with open("index.html", "w") as f:
+    f.write(f"<h1>Status {r.status_code}</h1>")
