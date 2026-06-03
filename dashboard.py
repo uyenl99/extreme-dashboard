8769a56dd3ccd1306b44fb0003a885487bce452a
uyenl99 <uyenl99@yahoo.com>
2026-06-03 15:25:37 -0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -7,13 +7,17 @@ headers = {
     "Authorization": f"Bearer {API_KEY}"
 }
 
-url = "https://api.collective2.com/world/apiv4"
+url = "https://api4-general.collective2.com/General/GetAccessKey"
 
 r = requests.get(url, headers=headers)
 
 print("STATUS:", r.status_code)
 print("TEXT:")
-print(r.text[:500])
+print(r.text[:2000])
 
-with open("index.html", "w") as f:
-    f.write(f"<h1>Status {r.status_code}</h1>")
+with open("index.html", "w", encoding="utf-8") as f:
+    f.write(f"""
+    <h1>API Test</h1>
+    <p>Status: {r.status_code}</p>
+    <pre>{r.text[:500]}</pre>
+    """)
