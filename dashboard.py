dab9b0f49f48a2e28fc7cfd2e1e7fb210654b903
uyenl99 <uyenl99@yahoo.com>
2026-06-04 14:08:17 -0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,5 +1,7 @@
 import os
 import requests
+import pandas as pd
+import plotly.graph_objects as go
 
 API_KEY = os.environ["C2_API_KEY"]
 
@@ -20,12 +22,51 @@ r = requests.get(
     params=params
 )
 
-print("STATUS:", r.status_code)
-print(r.text[:10000])
+data = r.json()
+
+daily = data["Results"][0]["DailyEquity"]
+
+df = pd.DataFrame(daily)
+
+df["Date"] = pd.to_datetime(df["Date"])
+
+fig = go.Figure()
+
+fig.add_trace(
+    go.Scatter(
+        x=df["Date"],
+        y=df["EquityWithCosts"],
+        mode="lines",
+        name="Equity With Costs"
+    )
+)
+
+chart_html = fig.to_html(
+    full_html=False,
+    include_plotlyjs="cdn"
+)
+
+html = f"""
+<html>
+<head>
+<title>Extreme Trading Dashboard</title>
+</head>
+
+<body>
+
+<h1>Extreme OS</h1>
+
+<h2>Equity Curve</h2>
+
+{chart_html}
+
+<p>
+Last Update: {df['Date'].max()}
+</p>
+
+</body>
+</html>
+"""
 
 with open("index.html", "w", encoding="utf-8") as f:
-    f.write(f"""
-    <h1>Daily Equity Test</h1>
-    <p>Status: {r.status_code}</p>
-    <pre>{r.text[:5000]}</pre>
-    """)
+    f.write(html)
